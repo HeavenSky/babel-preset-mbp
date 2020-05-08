4b6c12404a6af6192fad1733dade575a24b5c82f
youmo.lt <youmo.lt@antfin.com>
2020-05-08 10:36:46 +0800

    fix: 修复简写bug

--- stage-3.js
+++ stage-3.js
@@ -1,9 +1,9 @@
 module.exports = {
 	presets: [],
 	plugins: [
-		require("@babel/syntax-import-meta"),
-		require("@babel/syntax-dynamic-import"),
-		require("@babel/proposal-json-strings"),
-		[require("@babel/proposal-class-properties"), { loose: true }],
+		require("@babel/plugin-syntax-import-meta"),
+		require("@babel/plugin-syntax-dynamic-import"),
+		require("@babel/plugin-proposal-json-strings"),
+		[require("@babel/plugin-proposal-class-properties"), { loose: true }],
 	],
 };
